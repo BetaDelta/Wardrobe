94e267309fa775114b038ce92eec53abdfe89106
Rahul Dhodapkar <rahul.dhodapkar@vantagelocal.com>
2013-06-25 00:24:41 -0500

    large commit, initial structure created using Meteor.Router and Foundation framework for responsive UI.  Database elements also determined -- next stage to build functionality for web-app

--- wardrobe-src/Wardrobe.js
+++ wardrobe-src/Wardrobe.js
@@ -1,6 +1,46 @@
+Users = new Meteor.Collection("users");
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to Wardrobe.";
+
+	Meteor.startup(function() {
+		console.log("started at " + location.href);
+	});
+
+	Meteor.Router.add({
+		'/': 'home',
+
+		'/welcome': 'hello',
+
+		'/posts/:id':function(id) {
+			Session.set('postId', id);
+			return 'post'
+		}
+	});
+
+
+	/*
+	Meteor.Router.filters({
+		requireLogin: function(page) {
+			var username = Session.get('username');
+			if(username)
+				return page;
+			else
+				return 'sign_in';
+		}
+	
+	});
+
+	Meteor.Router.filter('requireLogin', {only: 'welcome'});
+  */
+
+
+	Template.hello.greeting = function () {
+		return "Welcome to Wardrobe, ";
+	};
+	
+  Template.hello.testing = function () {
+    var currUser = Users.findOne({"id":0}, {"name": 1});
+		return currUser;
   };
 
   Template.hello.events({
@@ -15,5 +55,13 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
-  });
+		if (Users.find().count() === 0) {
+			var names = ["Rahul",
+									 "Alan",
+									 "Brian"];
+			for(var i = 0; i < names.length; i++) {
+				Users.insert({name: names[i], id: i});
+			}
+		}
+	});
 }
